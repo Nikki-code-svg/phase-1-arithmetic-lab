224b20f4347fc877c5b86cbd8e36dc6e8ae388f1
nicole wachter <nicole.wachter6@gmail.com>
2024-06-08 20:51:01 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+const random = Math.floor(Math.random(0) * 10) + 1;
+let num3 = 32;
+let num4 = 7;
+const mod = num3 % num4;
+const max = Math.max(5, 10, 20);
